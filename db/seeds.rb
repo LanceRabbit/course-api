ccf7a89051f17425425e3d6cb8a764fb2f2f7245
Lancerabbit <lancerabbit@gmail.com>
2022-10-24 20:38:41 +0800

    chore(seed): add basic seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%w[langurage math programming algorithms].each do |course|
+  Course.create(title: course, instructor: "#{course} teacher", description: "Happy for Learning")
+end
+
+Course.all.each do |course|
+  1.upto(5) do |index|
+    course.chapters.create(title: "#{course.title} - Chapter #{index}", sequence: index)
+  end
+end
+
+Chapter.all.each do |chapter|
+  1.upto(5) do |index|
+    chapter.lessons.create(
+      title: "#{chapter.title} - Lesson #{index}",
+      description: "Lesson #{index} - description",
+      content: "This is Lesson #{index} content",
+      sequence: index
+    )
+  end
+end
